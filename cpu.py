736c0ae0bd7dfe9575bf66e3915af84899021327
Fabio Anderegg <fotcorn@gmail.com>
2011-09-23 13:38:34 +0200

    implemented minimal cpu

--- cpu.py
+++ cpu.py
@@ -1,57 +1,68 @@
 from instruction_set import instruction_set
-
-instr_set = {}
-
-for key, value in instruction_set.items():
-    instr_set[value] = key
-    
+   
 f = file('input/program.bin')
 lines = f.readlines()
 f.close()
 
 stack = []
+memory = {}
+instr_pointer = 0
 
-
-for i in range(0, len(lines)):
-    line = lines[i]
+while True:
+    instr = int(lines[instr_pointer])
     
-    if line == instr_set['add']:
+    if instr == instruction_set['add']:
         pass    
-    elif line == instr_set['sub']:
+    elif instr == instruction_set['sub']:
         pass
-    elif line == instr_set['div']:
+    elif instr == instruction_set['div']:
         pass
-    elif line == instr_set['mul']:
+    elif instr == instruction_set['mul']:
         pass
-    elif line == instr_set['mod']:
+    elif instr == instruction_set['mod']:
         pass
-    elif line == instr_set['inc']:
+    elif instr == instruction_set['inc']:
         pass
-    elif line == instr_set['or']:
+    elif instr == instruction_set['or']:
         pass
-    elif line == instr_set['and']:
+    elif instr == instruction_set['and']:
         pass
-    elif line == instr_set['not']:
+    elif instr == instruction_set['not']:
         pass
-    elif line == instr_set['xor']:
+    elif instr == instruction_set['xor']:
         pass
-    elif line == instr_set['pushaddr']:
+    elif instr == instruction_set['pushaddr']:
+        instr_pointer += 1
+        address = int(lines[instr_pointer])
+        stack.append(memory[address])
+    elif instr == instruction_set['pushconst']:
+        instr_pointer += 1
+        instr = int(lines[instr_pointer])
+        stack.append(instr)
+    elif instr == instruction_set['pop']:
         pass
-    elif line == instr_set['pushconst']:
+    elif instr == instruction_set['popaddr']:
+        instr_pointer += 1
+        address = int(lines[instr_pointer])
+        memory[address] = stack.pop()      
+    elif instr == instruction_set['jmp']:
+        instr_pointer += 1
+        pointer = int(lines[instr_pointer])
+        instr_pointer = pointer - 1
+    elif instr == instruction_set['jeq']:
         pass
-    elif line == instr_set['pop']:
+    elif instr == instruction_set['jne']:
         pass
-    elif line == instr_set['popaddr']:
+    elif instr == instruction_set['jlt']:
         pass
-    elif line == instr_set['jmp']:
+    elif instr == instruction_set['jgt']:
         pass
-    elif line == instr_set['jeq']:
-        pass
-    elif line == instr_set['jne']:
-        pass
-    elif line == instr_set['jlt']:
-        pass
-    elif line == instr_set['jgt']:
-        pass
-    elif line == instr_set['int']:
-        pass
+    elif instr == instruction_set['int']:
+        instr_pointer += 1
+        interrupt_nr = int(lines[instr_pointer])
+        if interrupt_nr == 1:
+            print stack.pop()
+    else:
+        print "unknown instruciton: " + hex(int(instr))
+                                            
+    instr_pointer += 1
